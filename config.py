51365117b9ba9ec4de748fd245ab29f1c88fef6c
JMGillum <gillum0927@gmail.com>
2023-12-12 15:53:27 -0800

    Removed unnecessary items

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-# Config file for Reddit scraper
-
 client_id="your_client_id" # your client id
 client_secret="your_client_secret" # your client secret
 
 # Example: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0
 user_agent="your_user_agent" # your user agent
 
-subReddits = ["subreddit1","subreddit2","subreddit3","etc."]
-
-desiredFlair = ["flair1","flair2","etc."] # Posts with at least one of these flairs will be kept
-desiredTitle = ["title1","title2","etc."] # Posts with at least one of these titles will be kept
+searches_file="searches.json" # Default is searches.json. Change this value to use a different file
