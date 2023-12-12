441674195ea6aea5a5579fadcfcbf72a4069823e
JMGillum <gillum0927@gmail.com>
2023-12-12 14:43:27 -0800

    Simplified

--- scroll.py
+++ scroll.py
@@ -29,65 +29,41 @@ class ScrollingList:
             self.currentLine = self.currentLine - numLines
     
     def getLines(self):
-        numLines = curses.LINES - self.tooltip.height()
-        lines = self.lines[self.currentLine:self.currentLine+numLines] + self.tooltip.returnLines()
-        lines[-1] = lines[-1][:-1]
+        lines = []
+        if(not self.tooltip == None):
+            numLines = curses.LINES - self.tooltip.height()
+            lines = self.lines[self.currentLine:self.currentLine+numLines]
+            while(len(lines) < numLines):
+                lines.append("")
+            lines = lines + self.tooltip.lines
+            if((len(lines) == curses.LINES) and (len(lines[-1]) >= curses.COLS)):
+                lines[-1] = lines[-1][:-1]
+        else:
+            numLines = curses.LINES
+            lines = self.lines[self.currentLine:self.currentLine+numLines]
+            while(len(lines) < numLines):
+                lines.append("")
+            if((len(lines) == curses.LINES) and (len(lines[-1]) >= curses.COLS)):
+                lines[-1] = lines[-1][:-1]
         return lines
+
+    def updateTooltip(self,tooltip):
+        if(not tooltip == None):
+            self.tooltip = tooltip
         
 
 
 class ToolTip:
-    def __init__(self,rows):
-        self.rows = []
-        for item in rows:
-            self.addRow(item)
-    
-    def addRow(self, row):
-        index = -1
-        sharedRow = False
-        if(len(self.rows) == 0):
-            self.rows.append(row)
-        else:
-            for item in self.rows:
-                if(item.y == row.y):
-                    if(row.x < item.x):
-                        item.string = row.string[:row.x + len(row.content)] + item.string[row.x + len(row.content):]
-                    else:
-                        item.string = item.string[:row.x] + row.string[row.x:row.x+len(row.content)] + item.string[row.x+len(row.content):]
-                    sharedRow = True
-                    break
-                elif (item.y > row.y):
-                    if(index == -1):
-                        index = 1
-                    else:
-                        index = index +1
-                
-                    
-            if(not sharedRow):
-                if(not index == -1):
-                    self.rows.insert(index,row)
-                else:
-                    self.rows.append(row)
+    def __init__(self,lines):
+        self.replace(lines)
 
-
-        
-    def returnRows(self):
-        return self.rows
-
-    def returnLines(self):
-        ls = []
-        for item in self.rows:
-            ls.append(item.string)
-        
-        return ls
+    def replace(self,lines):
+        self.lines = []
+        if(type(lines) == str):
+            self.lines.append(lines)
+        elif(type(lines) == list):
+            for item in lines:
+                self.lines.append(item)
 
     def height(self):
-        return len(self.rows)
-
-class Row:
-    def __init__(self, y, x, string,length=80):
-        self.y = y
-        self.x = x
-        self.content = string
-        self.string = placeString(string,length,self.x)
-    
+        return len(self.lines)
