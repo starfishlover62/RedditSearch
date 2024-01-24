86728eaf5cd0aae424df3f4724898b1fec34e344
JMGillum <gillum0927@gmail.com>
2024-01-23 20:21:35 -0800

    Function comments

--- dump.py
+++ dump.py
@@ -2,6 +2,10 @@ import json
 
 
 def serializeSub(sub):
+    """
+    Serializes a SubredditSearch object into a string format
+    * sub is the SubredditSearch object to be serialized
+    """
     string = f'"name":"{sub.name}", "whiteListTitle":{sub.titleWL}, "blackListTitle":{sub.titleBL}, "whiteListFlair":{sub.flairWL}, "blackListFlair":{sub.flairBL}, "whiteListPost":{sub.postWL}, "blackListPost":{sub.postBL}'
     string = "{" + string + "}"
     string = string.replace("'",'"')
@@ -9,6 +13,9 @@ def serializeSub(sub):
     return string
 
 def serializeSearch(search):
+    """
+    Serializes a single Search object into a string format
+    """
     string = f'"name":"{search.name}", "lastSearchTime":{search.lastSearchTime}, "subreddits":['
     for item in search.subreddits:
         string = string + serializeSub(item) + ","
@@ -22,6 +29,9 @@ def serializeSearch(search):
 
 
 def serializeSearches(searches):
+    """
+    Serializes a list of Search objects into a string format
+    """
     string = f'"searches":['
     for item in searches:
         string = string + serializeSearch(item) + ","
@@ -36,6 +46,9 @@ def serializeSearches(searches):
 
 
 def saveSearches(searches, filepath = "searches.json"):
+    """
+    Writes a list of searches to file.
+    """
     dumpStr = serializeSearches(searches)
     parsed = json.loads(dumpStr)
     with open(filepath,"w") as write:
