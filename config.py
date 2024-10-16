ac328f8d85995ce778a6b80f1d6e7fc4e0a21424
JMGillum <gillum0927@gmail.com>
2024-10-16 09:52:19 -0700

    Updated config to use correct useragent

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 client_id="your_client_id" # your client id
 client_secret="your_client_secret" # your client secret
-
-# Example: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0
-user_agent="your_user_agent" # your user agent
+user_agent="Python:better-search:v1.0.0 (by u/your_username)" # your user agent
 
 searches_file="searches.json" # Default is searches.json. Change this value to use a different file
-link_output="links.txt"
+link_output="links.txt" # This is where urls are saved when you request to save them
 
 fancy_characters=True # Default is True. Change to False if there are issues with rendering the following: '┌ ┐ └ ┘ ├ ┤ │ ─'
-
