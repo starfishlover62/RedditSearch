043abb509d24d2dc072efde86cf4d09dfdadb59e
starfishlover62 <jmgillum@nic.edu>
2025-02-20 11:37:58 -0800

    Attempted to fix bug when saving an empty search. Unsuccessful but good start

--- dump.py
+++ dump.py
@@ -6,46 +6,52 @@ def serializeSub(sub):
     Serializes a SubredditSearch object into a string format
     * sub is the SubredditSearch object to be serialized
     """
-    string = f'"name":"{sub.name}", "whiteListTitle":{sub.titleWL}, "blackListTitle":{sub.titleBL}, "whiteListFlair":{sub.flairWL}, "blackListFlair":{sub.flairBL}, "whiteListPost":{sub.postWL}, "blackListPost":{sub.postBL}'
-    string = "{" + string + "}"
-    string = string.replace("'", '"')
-    string = string.replace("None", "null")
-    return string
+    if sub is not None:
+        string = f'"name":"{sub.name}", "whiteListTitle":{sub.titleWL}, "blackListTitle":{sub.titleBL}, "whiteListFlair":{sub.flairWL}, "blackListFlair":{sub.flairBL}, "whiteListPost":{sub.postWL}, "blackListPost":{sub.postBL}'
+        string = "{" + string + "}"
+        string = string.replace("'", '"')
+        string = string.replace("None", "null")
+        return string
+    return ""
 
 
 def serializeSearch(search):
     """
     Serializes a single Search object into a string format
     """
-    string = f'"name":"{search.name}", "lastSearchTime":{search.lastSearchTime}, "subreddits":['
-    for item in search.subreddits:
-        string = string + serializeSub(item) + ","
+    if search is not None:
+        string = f'"name":"{search.name}", "lastSearchTime":{search.lastSearchTime}, "subreddits":['
+        for item in search.subreddits:
+            string = string + serializeSub(item) + ","
 
-    string = string.rstrip(",")
-    string = string + "]"
-    string = "{" + string + "}"
-    string = string.replace("'", '"')
-    string = string.replace("None", "null")
-    return string
+        string = string.rstrip(",")
+        string = string + "]"
+        string = "{" + string + "}"
+        string = string.replace("'", '"')
+        string = string.replace("None", "null")
+        return string
+    return ""
 
 
 def serializeSearches(searches):
     """
     Serializes a list of Search objects into a string format
     """
-    if not isinstance(searches, list):
-        searches = [searches]
-    string = '"searches":['
-    for item in searches:
-        string = string + serializeSearch(item) + ","
+    if searches is not None:
+        if not isinstance(searches, list):
+            searches = [searches]
+        string = '"searches":['
+        for item in searches:
+            string = string + serializeSearch(item) + ","
 
-    string = string.rstrip(",")
-    string = string + "]"
-    string = "{" + string + "}"
-    string = string.replace("'", '"')
-    string = string.replace("None", "null")
+        string = string.rstrip(",")
+        string = string + "]"
+        string = "{" + string + "}"
+        string = string.replace("'", '"')
+        string = string.replace("None", "null")
 
-    return string
+        return string
+    return ""
 
 
 def saveSearches(searches, filepath="searches.json"):
