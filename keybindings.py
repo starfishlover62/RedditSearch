4b47f25510b7f32e80ea0fa879054be1da3852c8
starfishlover62 <jmgillum@nic.edu>
2025-02-20 11:38:33 -0800

    Split keybindings into more logical groupings

--- keybindings.py
+++ keybindings.py
@@ -1,29 +1,42 @@
 import curses
 # Used for scrolling through post headers and a post
-scrollKeys={ord("w"):"scrollUp",
-            ord("s"):"scrollDown",
-            ord("a"):"scrollLeft",
-            ord("d"):"scrollRight",
-            ord("t"):"scrollTop",
-            ord("b"):"scrollBottom",
-            curses.KEY_UP:"scrollUp",
-            curses.KEY_DOWN:"scrollDown",
-            curses.KEY_LEFT:"scrollLeft",
-            curses.KEY_RIGHT:"scrollRight"}
+scrollVerticalKeys={
+    ord("w"):"scrollUp",
+    ord("s"):"scrollDown",
+    ord("t"):"scrollTop",
+    ord("b"):"scrollBottom",
+    curses.KEY_UP:"scrollUp",
+    curses.KEY_DOWN:"scrollDown"
+}
+# Used for scrolling between posts
+scrollHorizontalKeys={
+    ord("a"):"scrollLeft",
+    ord("d"):"scrollRight",
+    curses.KEY_LEFT:"scrollLeft",
+    curses.KEY_RIGHT:"scrollRight"
+}
 
 # Used for controlling the terminal screen
-controlKeys={ord("q"):"exit",
-            ord("r"):"refresh",
-            curses.KEY_RESIZE:"resize"}
+controlKeys={
+    ord("q"):"exit",
+    ord("r"):"refresh",
+    curses.KEY_RESIZE:"resize"
+}
 
 # Used for interacting with a post
-postKeys={  ord("h"):"help",
-            ord("o"):"open",
-            ord("c"):"copy",
-            ord("m"):"message",
-            ord("u"):"url",
-            ord("i"):"image"}
+postKeys={ 
+    ord("h"):"help",
+    ord("o"):"open",
+    ord("c"):"copy",
+    ord("m"):"message",
+    ord("u"):"url",
+    ord("i"):"image"
+}
 
 # Used for selecting a search and editing searches
-treekKeys={ ord("e"):"enter",
-            ord("v"):"view"}
+editKeys={
+    ord("e"):"enter",
+    ord("v"):"view",
+    ord("a"):"add",
+    ord("d"):"delete"
+}
